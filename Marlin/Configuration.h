f7e0bfdb1b6d1ca86b72c960eed7fdf7b9e592ee
fponzo <52773814+fponzo@users.noreply.github.com>
2020-06-13 14:24:07 +0200

    13-Jun-2020
    Remove Hybrid threshold
    Adjust _MINTEMPs

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -335,11 +335,11 @@
 // to check that the wiring to the thermistor is not broken.
 // Otherwise this would lead to the heater being powered on all the time.
 #define HEATER_0_MINTEMP 5
-#define HEATER_1_MINTEMP 0
-#define HEATER_2_MINTEMP 0
-#define HEATER_3_MINTEMP 0
-#define HEATER_4_MINTEMP 0
-#define BED_MINTEMP 0
+#define HEATER_1_MINTEMP 5
+#define HEATER_2_MINTEMP 5
+#define HEATER_3_MINTEMP 5
+#define HEATER_4_MINTEMP 5
+#define BED_MINTEMP 5
 
 // When temperature exceeds max temp, your heater will be switched off.
 // This feature exists to protect your hotend from overheating accidentally, but *NOT* from thermistor short/failure!

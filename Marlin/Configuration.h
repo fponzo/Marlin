8a4ee79593129ad6ab4ec5e1be31e339d19936b1
fponzo <52773814+fponzo@users.noreply.github.com>
2020-10-31 15:53:50 +0100

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -77,7 +77,7 @@
 #define STRING_CONFIG_H_AUTHOR "(LuckyStriker, Ender-5 with TMC2130, BLTouch and UBL)" // Who made the changes.
 #define SHOW_BOOTSCREEN
 #define STRING_SPLASH_LINE1 "1.1.10 31-Oct-20 rev1" // will be shown during bootup in line 1
-#define STRING_SPLASH_LINE2 "2130/V6/BLT/UBL/BMC"   // will be shown during bootup in line 2
+#define STRING_SPLASH_LINE2 "2130/uSwissDD/BLT/UBL"   // will be shown during bootup in line 2
 
 /**
  * *** VENDORS PLEASE READ ***
